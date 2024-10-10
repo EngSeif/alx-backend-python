d1a994849a0e0ff2b2c857c528828a0837dba4e0
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-11 01:10:28 +0300

    Py Type Anno | List Floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+5-Sum  :
+    sixth Task of the project
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    sum_list  :
+    input_list of floats as argument
+    and returns their sum as a float.
+    """
+    return sum(input_list)
