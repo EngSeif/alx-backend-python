c31426f2874a20d2b8414297fa9af4578cfe767a
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-11 01:16:37 +0300

    Py Type Anno | List Mixed

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+5-Sum  :
+    sixth Task of the project
+"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    sum_list  :
+    takes a list mxd_lst of integers
+    and floats and returns their
+    sum as a float.
+    """
+    return sum(mxd_lst)
