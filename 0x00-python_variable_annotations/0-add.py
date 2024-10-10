4994428f720532b04278fe1345dcbb1bf04cc225
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-11 00:17:52 +0300

    Py Type Anno | Basic

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+0-add :
+    first Task of the projetc
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    add :
+    Take Two floats and return their sum
+    """
+    return a + b
