37ec67ba0535c272c620e65a817ffbeb52fefb42
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-11 00:22:55 +0300

    Py Type Anno | Basic concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+1-concat :
+    second Task of the project
+"""
+
+
+def concat (str1: str, str2: str) -> str:
+    """
+    add :
+    Take Two strings and return their concat
+    """
+    return str1 + str2
