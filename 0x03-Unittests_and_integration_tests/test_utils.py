99b4fbff9cb2710e56b045c8e2bae3b8536b207f
EngSeif <eng.seifmohamed2004@gmail.com>
2024-11-01 14:15:47 +0200

    Unittest | parametrized with raise Error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,6 +7,7 @@ from utils import access_nested_map
 import unittest
 from parameterized import parameterized  # Import directly
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """
     Class to Test AccessNestedMap
@@ -20,5 +21,18 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, a, b, expected):
         """
         parameterized test
+        Assert Equal
         """
         self.assertEqual(access_nested_map(a, b), expected)
+
+    @parameterized.expand([
+        ({}, ["a"]),
+        ({"a": 1}, ["a", "b"])
+    ])
+    def test_access_nested_map_exception(self, a, b):
+        """
+        parameterized test
+        Assert Raises
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(a, b)
